d1fc6e9c901ecc8dd35b1c7d948f30c58edfe8df
Yesid-Ackerman <deironyesithym21@gmail.com>
2024-09-06 16:38:44 -0500

    end point crear reseñas

--- app/Models/Connection.php
+++ app/Models/Connection.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Conexion extends Model
+class Connection extends Model
 {
     
 
