9b61fc8dd047824789375fa822e27b1701d9f5e2
sh4ka11 <dayronhernandezparedes@gmail.com>
2024-11-22 09:52:44 -0500

    FIX-5

--- database/migrations/2024_08_14_024552_create_EntrepreneursList_table.php
+++ database/migrations/2024_08_14_024552_create_EntrepreneursList_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('EntrepreneursLists', function (Blueprint $table) {
+        Schema::create('EntrepreneursList', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('entrepreneurs_id')->nullable();
             $table->foreign('entrepreneurs_id')
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('EntrepreneurLists');
+        Schema::dropIfExists('EntrepreneurList');
     }
 };
