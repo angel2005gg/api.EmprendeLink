9bf20209c6d86f3e7d666cef71a8e04fe47cdcd3
Sebastian-Paz <esdp5432@gmail.com>
2024-11-30 18:29:19 +1400

    registro melo 😎🤠

--- database/migrations/2024_05_15_024245_create_investors_table.php
+++ database/migrations/2024_05_15_024245_create_investors_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('investors', function (Blueprint $table) {
             $table->id();
-            
+
             // Clave foránea a la tabla users
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
             $table->timestamps();
         });
     }
