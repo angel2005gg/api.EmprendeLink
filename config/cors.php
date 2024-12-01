f9bb01b086f59c2193b44d1d52ebab3069137af4
sh4ka11 <dayronhernandezparedes@gmail.com>
2024-11-30 19:04:10 -0500

    probando

--- config/cors.php
+++ config/cors.php
@@ -2,17 +2,31 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:8000',
-        'http://localhost:8001',
-        'https://127.0.0.1:8000',
-        'https://127.0.0.1:8001',
-        'https://clienteemprendelink-production.up.railway.app',
-    ],
+'allowed_origins' => [
+    'https://clienteemprendelink-production.up.railway.app',
+    'http://localhost:8000', // API
+    'http://localhost:8001', // Frontend
+    'https://127.0.0.1:8000', // API en caso de usar HTTPS
+    'https://127.0.0.1:8001', // Frontend en caso de usar HTTPS
+    'http://clienteemprendelink-production.up.railway.app',
+],
 
     'allowed_origins_patterns' => [],
 
@@ -22,5 +36,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false, // Cambia a true si usas cookies o autenticación basada en sesión
+    'supports_credentials' => false,
+
 ];
