9bf288b3ba5249ce1e45271dd5ec5bdfd987d55b
sh4ka11 <dayronhernandezparedes@gmail.com>
2024-12-04 18:27:14 -0500

    usser

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -28,7 +28,7 @@ class UserController extends Controller
             'birth_date' => 'required|date',
             'password' => 'required|confirmed|min:8',
             'phone' => 'required|integer|max:255',
-            'pic_profile' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Imagen opcional
+            'pic_profile' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Imagen opcional
             'email' => 'required|string|email|max:255|unique:users',
             'location' => 'required|string|max:255',
             'number' => 'required|integer|max:255',
@@ -95,7 +95,7 @@ class UserController extends Controller
                 'birth_date' => 'required|date',
                 'password' => 'required|confirmed|min:8',
                 'phone' => 'required|integer|max:255',
-                'pic_profile' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Imagen opcional
+                'pic_profile' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Imagen opcional
                 'email' => 'required|string|email|max:255|unique:users',
                 'location' => 'required|string|max:255',
                 'number' => 'required|integer|max:255',
