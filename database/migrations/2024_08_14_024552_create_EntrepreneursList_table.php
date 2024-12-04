78e45e28ee02c35577bb01eb4a6068f7add90ec6
k2035ortiz <kevinalexisortizcalvahe@gmail.com>
2024-12-04 16:45:00 -0500

    k3

--- database/migrations/2024_08_14_024552_create_EntrepreneursList_table.php
+++ database/migrations/2024_08_14_024552_create_EntrepreneursList_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        
         Schema::create('EntrepreneursList', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('entrepreneurs_id')->nullable();
